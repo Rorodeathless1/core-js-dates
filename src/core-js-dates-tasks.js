99315bb0dbfc5fc557d7638053e0f29a56a6e878
Rorodeathless1 <romochka-suvorov-2016@mail.ru>
2025-12-09 15:05:35 +0300

    add tack 6

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -97,8 +97,10 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month, 0);
+
+  return date.getDate();
 }
 
 /**
@@ -112,8 +114,15 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const MS_PER_DAY = 1000 * 60 * 60 * 24;
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const timeStart = start.getTime();
+  const timeEnd = end.getTime();
+  const timeDifference = timeEnd - timeStart;
+  const daysDifference = Math.round(timeDifference / MS_PER_DAY);
+  return daysDifference + 1;
 }
 
 /**
