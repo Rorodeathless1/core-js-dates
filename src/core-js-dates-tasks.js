cd17665031444cfd66d55f52b43d6f2cd8255e82
Rorodeathless1 <romochka-suvorov-2016@mail.ru>
2025-12-09 15:16:26 +0300

    add tack 7

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -142,8 +142,16 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const checkDate = new Date(date);
+  const startDate = new Date(period.start);
+  const endDate = new Date(period.end);
+
+  const timeToCheck = checkDate.getTime();
+  const timeStart = startDate.getTime();
+  const timeEnd = endDate.getTime();
+
+  return timeToCheck >= timeStart && timeToCheck <= timeEnd;
 }
 
 /**
